eb16a1790b648c32aa02611fa96de87138a2f6f5
Arteks718 <artexbro.official@gmail.com>
2022-11-11 16:34:40 +0200

    getApiKey

--- index.js
+++ index.js
@@ -1,173 +1,225 @@
 const Telegraf = require("telegraf").Telegraf,
   os = require("node:os"),
-  BOT_TOKEN = "5687253547:AAHqiWGBpGg_dgW_kLgamFBEJkio-eqceI8"
-API_KEY = "SEdm8VmLyERyvdTunO";
-bybitSecret = "Tcy325WsyTIriwORN3GRRIluceBYPY5n4Jys";
-const bot = new Telegraf(BOT_TOKEN);
-const { time } = require("node:console");
-const { Keyboard } = require("telegram-keyboard");
-const url = "https://api-testnet.bybit.com/v2/public";
+  BOT_TOKEN = "5687253547:AAHqiWGBpGg_dgW_kLgamFBEJkio-eqceI8",
+  bot = new Telegraf(BOT_TOKEN),
+  { Keyboard } = require("telegram-keyboard");
 
-var timeFromServer = 0;
-let timeNow = 0;
-let sign = "";
-bot.start((ctx) => {
-  ctx.reply("Вітаємо в нашому боті!");
-  const keyboard = Keyboard.make([
-    ["Button 1", "Button 2"], // First row
-    ["Button 3", "Button 4"], // Second row
-  ]);
-  ctx.reply("Simple built-in keyboard", keyboard.reply());
-  // ctx.replyWithHTML("Welcome", {
-  //   reply_markup: {
-  //     inline_keyboard: [
-  //       [{ text: "Get last statistic", url: "https://russianwarship.rip" }],
-  //       [{ text: "Get all by day", callback_data: "getAllByDay" }],
-  //       [{ text: "Get all statistic", callback_data: "getAll" }],
-  //     ],
-  //   },
-  // });
+
+
+// let API_KEY = "SEdm8VmLyERyvdTunO";
+// let API_SECRET = "Tcy325WsyTIriwORN3GRRIluceBYPY5n4Jys";
+
+const {
+  InverseClient,
+  LinearClient,
+  InverseFuturesClient,
+  SpotClient,
+  SpotClientV3,
+  UnifiedMarginClient,
+  USDCOptionClient,
+  USDCPerpetualClient,
+  AccountAssetClient,
+  CopyTradingClient, } = require('bybit-api');
+
+let chooseApiKey;
+let API_KEY = "";
+let API_SECRET = "";
+const useTestnet = false;
+const recvWindow = 5000;
+const keyboardApiYes = Keyboard.make(
+  ["Symbol", "Order book", "Query Kline", "Latest Big Deal", "Get Active Order", "Cancel Active Order", "Place Active Order", "Cancel All Active Orders", "Get Wallet Balance"], {
+    pattern: [4, 2, 2]
+  });
+const keyboardApiNo = Keyboard.make(
+  ["Symbol", "Order book", "Query Kline", "Latest Big Deal"]);
+
+
+bot.start(async (ctx) => {
+  ctx.reply("Test", Keyboard.remove())
+  await ctx.reply("Привіт, вітаю тебе у боті CryptoBybitBot!");
+  await ctx.reply("Для працездібності більшості функцій тобі потрібно ввести свій API Key та API Secret Key ");
+    ctx.replyWithHTML("Чи бажаєте ви ввести ключі?", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Так", callback_data: "yesAPI" }],
+          [{ text: "Ні", callback_data: "noAPI" }],
+        ],
+      },
+    });
+})
+
+bot.command('info', ctx => {
+  bot.telegram.sendMessage(ctx.chat.id, "Bot Info", {
+    reply_markup: {
+      keyboard: [
+        [
+          { text: "Credits"},
+          { text: "API" }
+        ]
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  })
+})
+
+function getKeyboard(choose)
+{
+
+}
+
+
+
+
+const clientInverse = new InverseClient({
+  key: API_KEY,
+  secret: API_SECRET,
+  testnet: useTestnet
 });
 
-function getTimeFromServer() {
-  return fetch("https://api.bybit.com/v2/public/time", {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
+bot.hears("apikey", async (ctx) => {
+  if(API_KEY != "" && API_SECRET != "")
+  {
+    await ctx.reply(API_KEY);
+    await ctx.reply(API_SECRET);
+    console.log(API_KEY.length);
+    console.log(API_SECRET.length);
+    console.log(API_KEY);
+    console.log(API_SECRET);
+  }else{
+    ctx.reply("No");
+  }
+})
+
+function getTickers(symbol)
+{
+  clientInverse.getTickers({symbol: symbol})
+  .then(result => {
+    console.log("getTickers result: ", result);
   })
-    .then((res) => res.json())
-    .then((data) => {
-      let recv_window = 5000;
-      timeNow = Date.now();
-      timeFromServer = data.time_now;
-      if (timeFromServer - recv_window <= timeNow < timeFromServer + 1000) {
-        timeFromServer = timeNow; 
-      } else {
-        console.log("Error timestamp");
+  .catch(err =>{
+    console.error("getTickers error: ", err);
+  })
+}
+
+
+
+// clientInverse.getWalletBalance({coin: "BTC"})
+//   .then(result => {
+//     console.log("getWalletBalance result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getWalletBalance error: ", err);
+//   });
+
+  // requestLibraryOptions
+
+
+// For public-only API calls, simply don't provide a key & secret or set them to undefined
+// const client = new InverseClient({});
+
+
+// clientInverse.getServerTime()
+//   .then(result => {
+//     console.log("getServerTime result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getServerTime error: ", err);
+//   });
+
+
+
+// clientInverse.getTickers({symbol: "APTUSDT"})
+//   .then(result => {
+//     console.log("getTickers result: ", result);
+//   })
+//   .catch(err =>{
+//     console.error("getTickers error: ", err);
+//   })
+// clientInverse.getApiKeyInfo()
+//   .then(result => {
+//     console.log("getApiKeyInfo result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getApiKeyInfo error: ", err);
+//   });
+// clientInverse.getKline({symbol:"BTCUSD", interval:"60", from: 1667018735})
+//   .then(result => {
+//     console.log("getKline result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getKline error: ", err);
+//   });
+
+
+
+
+
+// client.getLongShortRatio({symbol: "APTUSD", period:"5min"})
+//   .then(result => {
+//     console.log("getApiKeyInfo result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getApiKeyInfo error: ", err);
+//   });
+// client.getOrderBook({ symbol: 'BTCUSD' })
+//   .then(result => {
+//     console.log("getOrderBook result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getOrderBook error: ", err);
+//   });
+
+// clientUnified.getBalances({coin: "USDT"})
+//   .then(result => {
+//     console.log("getBalances result: ", result);
+//   })
+//   .catch(err => {
+//     console.error("getBalances error: ", err);
+//   });
+
+
+// Get apikey and api secret key
+bot.action("yesAPI", async (ctx) => {
+  bot.on
+  ctx.deleteMessage (ctx.inlineMessageId);
+  if(API_KEY == "" && API_SECRET == "")
+  {
+    ctx.reply("Введіть APIKey:APISecret");
+    bot.hears(/^[A-Za-z0-9а-яёі]/, async (ctx) => {
+      message = ctx.message.text;
+      var arrayOfStrings = message.split(":");
+      API_KEY = arrayOfStrings[0];
+      API_SECRET = arrayOfStrings[1];
+      if(API_KEY.length == 18)
+      {
+        await ctx.reply("Ваш API Key: " + API_KEY);
+        if(API_SECRET.length == 36)
+        {
+          await ctx.reply("Ваш API Secret: " + API_SECRET);
+          chooseApiKey = true;
+        }else{ctx.reply("Неправильний формат API Secret");}
+      }else{ctx.reply("Неправильний формат API Key");}
+      if(chooseApiKey == true)
+      {
+        ctx.reply("Тепер ви можете користуватись функціями бота", keyboardApiYes.reply());
       }
     })
-    .catch((er) => {
-      console.log("Error: ${er}");
-    });
-}
 
-function getSignature(parameters, secret) {
-  let orderedParams = "";
-  var crypto = require("crypto");
-  Object.keys(parameters)
-    .sort()
-    .forEach(function (key) {
-      orderedParams += key + "=" + parameters[key] + "&";
-    });
-  orderedParams = orderedParams.substring(0, orderedParams.length - 1);
-  return crypto
-    .createHmac("sha256", secret)
-    .update(orderedParams)
-    .digest("hex");
-}
+  }else{
+    ctx.reply("Ваші ключі вже було введено\n", + API_KEY + "\n" + API_SECRET);
+  }
+})
 
-async function getSign() {
-  await getTimeFromServer();
-  const queryParams = {
-    api_key: API_KEY,
-    coin: "BTC",
-    timestamp: timeFromServer,
-  };
-  const queryString = JSON.stringify(queryParams);
-  sign = getSignature(queryParams, bybitSecret);
-}
-bot.hears("sign", (ctx) => {
-  console.log(getSign());
-});
-
-bot.hears("timestamp", async (ctx) => {
-  await getTimeFromServer();
-  console.log(timeFromServer);
-  console.log(timestamp);
-  ctx.reply(String(timestamp));
-});
-
-bot.hears("balance", async (ctx) => { 
-  await getSign();
-  console.log( "https://api-testnet.bybit.com/v2/private/wallet/balance?api_key=" + API_KEY + "&coin=BTC&timestamp=" + timeFromServer + "&sign=" + sign + "");
-  fetch(
-    "https://api.bybit.com/v2/private/wallet/balance?api_key=" + API_KEY + "&coin=BTC&timestamp=" + timeFromServer + "&sign=" + sign
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log(timeFromServer);
-      ctx.reply(sign);
-    });
-});
-
-bot.hears(/[A-Z]+/i, (ctx) => {
-  let message = ctx.message.text;
-  message = message.toUpperCase();
-  console.log(message);
-  fetch("https://api-testnet.bybit.com/v2/public/tickers", {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      data_from_server = data.result;
-      data_from_server.forEach((element) => {
-        if (element.symbol == message) {
-          let tickDirection = element.last_tick_direction;
-          console.log(element.symbol);
-          console.log(tickDirection);
-          // switch (tickDirection) {
-          //   case "PlusTick":
-          //     tickDirection = "Ціна збільшується";
-          //   case "MinusTick":
-          //     tickDirection = "Ціна зменьшується";
-          //   case "ZeroPlusTick":
-          //     tickDirection = "Ціна згоди більше ніж попередньої згоди";
-          //   case "ZeroMinusTick":
-          //     tickDirection = "Ціна згоди менше ніж попередньої згоди";
-          // }
-          if (element.last_tick_direction == "PlusTick") {
-            tickDirection = "Ціна збільшується";
-          } else if (element.last_tick_direction == "MinusTick") {
-            tickDirection = "Ціна зменьшується";
-          } else if (element.last_tick_direction == "ZeroPlusTick") {
-            tickDirection = "Ціна згоди більше ніж попередньої згоди";
-          } else if (element.last_tick_direction == "ZeroMinusTick") {
-            tickDirection = "Ціна згоди менше ніж попередньої згоди";
-          }
-          ctx.replyWithHTML(
-            "<b>Символ: </b>" +
-              element.symbol +
-              "\n<b>Ціна індексу: </b>" +
-              element.index_price +
-              "$" +
-              "\n<b>Напрямок ціни: </b>" +
-              tickDirection +
-              "\n<b>Найвища ціна за 24 год: </b>" +
-              element.high_price_24h +
-              "$" +
-              "\n<b>Найнижча ціна за 24 год: </b>" +
-              element.low_price_24h +
-              "$" +
-              "\n<b>Процентна зміна ціни відносно 1 год: </b>" +
-              element.price_24h_pcnt +
-              "%" +
-              "\n<b>Ціна 24 год тому: </b>" +
-              element.prev_price_24h +
-              "$" +
-              "\n<b>Оборот за 24 год: </b>" +
-              element.turnover_24h +
-              "\n<b>Об'єм за 24 год: </b>" +
-              element.volume_24h
-          );
-        }
-      });
-    })
-    .catch((er) => {
-      console.log("Error: ${er}");
-    });
-});
-
-const queryParams = {};
+// send messege if user doesn't want enter api key and secret key
+bot.action("noAPI", (ctx) => {
+  ctx.deleteMessage(ctx.inlineMessageId);
+  chooseApiKey = false;
+  ctx.reply("Добре, якщо передумаєте, то зможете у будь-який час додати свої ключі");
+  if(chooseApiKey == false)
+  {
+    ctx.reply("Тепер ви можете користуватись функціями бота", keyboardApiNo.reply());
+  }
+})
 
 bot.launch();
---
+++ index1.js
@@ -0,0 +1,172 @@
+const Telegraf = require("telegraf").Telegraf,
+  os = require("node:os"),
+  BOT_TOKEN = "5687253547:AAHqiWGBpGg_dgW_kLgamFBEJkio-eqceI8"
+API_KEY = "SEdm8VmLyERyvdTunO";
+bybitSecret = "Tcy325WsyTIriwORN3GRRIluceBYPY5n4Jys";
+const bot = new Telegraf(BOT_TOKEN);
+const { time } = require("node:console");
+const { Keyboard } = require("telegram-keyboard");
+const url = "https://api-testnet.bybit.com/v2/public";
+
+var timeFromServer = 0;
+let timeNow = 0;
+let sign = "";
+bot.start((ctx) => {
+  ctx.reply("Вітаємо в нашому боті!");
+  const keyboard = Keyboard.make([
+    ["Button 1", "Button 2"], // First row
+    ["Button 3", "Button 4"], // Second row
+  ]);
+  ctx.reply("Simple built-in keyboard", keyboard.reply());
+  // ctx.replyWithHTML("Welcome", {
+  //   reply_markup: {
+  //     inline_keyboard: [
+  //       [{ text: "Get last statistic", url: "https://russianwarship.rip" }],
+  //       [{ text: "Get all by day", callback_data: "getAllByDay" }],
+  //       [{ text: "Get all statistic", callback_data: "getAll" }],
+  //     ],
+  //   },
+  // });
+});
+
+function getTimeFromServer() {
+  return fetch("https://api.bybit.com/v2/public/time", {
+    method: "get",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let recv_window = 5000;
+      timeNow = Date.now();
+      timeFromServer = data.time_now;
+      if (timeFromServer - recv_window <= timeNow < timeFromServer + 1000) {
+        timeFromServer = timeNow; 
+      } else {
+        console.log("Error timestamp");
+      }
+    })
+    .catch((er) => {
+      console.log("Error: ${er}");
+    });
+}
+
+function getSignature(parameters, secret) {
+  let orderedParams = "";
+  var crypto = require("crypto");
+  Object.keys(parameters)
+    .sort()
+    .forEach(function (key) {
+      orderedParams += key + "=" + parameters[key] + "&";
+    });
+  orderedParams = orderedParams.substring(0, orderedParams.length - 1);
+  return crypto
+    .createHmac("sha256", secret)
+    .update(orderedParams)
+    .digest("hex");
+}
+
+async function getSign() {
+  await getTimeFromServer();
+  const queryParams = {
+    api_key: API_KEY,
+    coin: "USDT",
+    timestamp: timeFromServer,
+  };
+  const queryString = JSON.stringify(queryParams);
+  sign = getSignature(queryParams, bybitSecret);
+}
+bot.hears("sign", (ctx) => {
+  console.log(getSign());
+});
+
+bot.hears("timestamp", async (ctx) => {
+  await getTimeFromServer();
+  console.log(timeFromServer);
+  console.log(timestamp);
+  ctx.reply(String(timestamp));
+});
+
+bot.hears("balance", async (ctx) => { 
+  await getSign();
+  console.log( "https://api-testnet.bybit.com/v2/private/wallet/balance?api_key=" + API_KEY + "&coin=BTC&timestamp=" + timeFromServer + "&sign=" + sign + "");
+  fetch(
+    "https://api.bybit.com/v2/private/wallet/balance?api_key=" + API_KEY + "&coin=USDT&timestamp=" + timeFromServer + "&sign=" + sign
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      ctx.reply(sign);
+    });
+});
+
+bot.hears(/[A-Z]+/i, (ctx) => {
+  let message = ctx.message.text;
+  message = message.toUpperCase();
+  console.log(message);
+  fetch("https://api-testnet.bybit.com/v2/public/tickers", {
+    method: "get",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      data_from_server = data.result;
+      data_from_server.forEach((element) => {
+        if (element.symbol == message) {
+          let tickDirection = element.last_tick_direction;
+          console.log(element.symbol);
+          console.log(tickDirection);
+          // switch (tickDirection) {
+          //   case "PlusTick":
+          //     tickDirection = "Ціна збільшується";
+          //   case "MinusTick":
+          //     tickDirection = "Ціна зменьшується";
+          //   case "ZeroPlusTick":
+          //     tickDirection = "Ціна згоди більше ніж попередньої згоди";
+          //   case "ZeroMinusTick":
+          //     tickDirection = "Ціна згоди менше ніж попередньої згоди";
+          // }
+          if (element.last_tick_direction == "PlusTick") {
+            tickDirection = "Ціна збільшується";
+          } else if (element.last_tick_direction == "MinusTick") {
+            tickDirection = "Ціна зменьшується";
+          } else if (element.last_tick_direction == "ZeroPlusTick") {
+            tickDirection = "Ціна згоди більше ніж попередньої згоди";
+          } else if (element.last_tick_direction == "ZeroMinusTick") {
+            tickDirection = "Ціна згоди менше ніж попередньої згоди";
+          }
+          ctx.replyWithHTML(
+            "<b>Символ: </b>" +
+              element.symbol +
+              "\n<b>Ціна індексу: </b>" +
+              element.index_price +
+              "$" +
+              "\n<b>Напрямок ціни: </b>" +
+              tickDirection +
+              "\n<b>Найвища ціна за 24 год: </b>" +
+              element.high_price_24h +
+              "$" +
+              "\n<b>Найнижча ціна за 24 год: </b>" +
+              element.low_price_24h +
+              "$" +
+              "\n<b>Процентна зміна ціни відносно 1 год: </b>" +
+              element.price_24h_pcnt +
+              "%" +
+              "\n<b>Ціна 24 год тому: </b>" +
+              element.prev_price_24h +
+              "$" +
+              "\n<b>Оборот за 24 год: </b>" +
+              element.turnover_24h +
+              "\n<b>Об'єм за 24 год: </b>" +
+              element.volume_24h
+          );
+        }
+      });
+    })
+    .catch((er) => {
+      console.log("Error: ${er}");
+    });
+});
+
+const queryParams = {};
+
+bot.launch();
