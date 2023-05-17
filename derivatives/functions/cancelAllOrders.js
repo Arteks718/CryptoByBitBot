ddaf6181d46ca3e092a49175a24a61c75f565739
Arteks718 <artexbro.official@gmail.com>
2023-05-17 10:39:11 +0300

    cancelOrder, cancelAllOrders

---
+++ derivatives/functions/cancelAllOrders.js
@@ -0,0 +1,43 @@
+const { bot, users } = require("../../config");
+const { RestClientV5 } = require("bybit-api");
+
+module.exports = async (ctx) => {
+  const user = await users.findOne({
+    idTelegram: ctx.chat.id,
+    chooseButtonAPI: true,
+    apiKey: { $exists: true },
+    status: "cancelAllOrdersDirevatives",
+  });
+  if(user){
+    ctx.reply("Введіть символ за яким будуть видалятись усі замовлення")
+    const clientByBit = new RestClientV5({
+      key: user.apiKey,
+      secret: user.apiSecret,
+      testnet: false,
+      recv_window: 5000,
+    });
+
+    bot.hears(/^[A-Za-z]+$/, async (ctx) => {
+      clientByBit.cancelAllOrders({category: 'linear', symbol: ctx.message.text.toUpperCase()})
+        .then(async result => {
+          if(result.retCode == 0) {
+            await ctx.reply("✅Операція видалення усіх замовлень успішна✅");
+            let listString = `Список усіх видаленних замовлень:`;
+            result.result.list.forEach((order) => {
+              if(order.orderId)
+                listString += `\n\nІдентифікатор замовлення: ${order.orderId}`
+              if(order.orderLinkId)
+                listString += `\nКористувацький ідентифікатор замовлення: ${order.orderLinkId}`
+            })
+            await ctx.replyWithHTML(listString);
+          } else
+              throw new Error(result.retCode);
+        })
+        .catch((err) => {
+          ctx.reply("❌Помилка видалення усіх замовлень");
+          console.log(err)
+        });
+    })
+  }else
+    ctx.reply("❌Помилка, функція не обрана❌")
+}
---
+++ derivatives/functions/cancelOrder.js
@@ -0,0 +1,38 @@
+const { bot, users } = require("../../config");
+const { RestClientV5 } = require("bybit-api");
+
+module.exports = async (ctx) => {
+  const user = await users.findOne({
+    idTelegram: ctx.chat.id,
+    chooseButtonAPI: true,
+    apiKey: { $exists: true },
+    status: "cancelOrderDirevatives",
+  });
+  if(user) {
+    ctx.reply("Введіть символ та номер ордера (отримати його за командою Get Open Orders)");
+    const clientByBit = new RestClientV5({
+      key: user.apiKey,
+      secret: user.apiSecret,
+      testnet: false,
+      recv_window: 5000,
+    });
+
+    bot.hears(/^[A-Za-z]+:[a-fA-F0-9]{8}-(?:[a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}$/, (ctx) => {
+      const arrayOfStrings = ctx.message.text.split(":");
+      const symbol = arrayOfStrings[0];
+      const orderId = arrayOfStrings[1];
+      clientByBit.cancelOrder({ category: 'linear', symbol: symbol.toUpperCase(), orderId: orderId })
+        .then(result => {
+          if(result.retCode == 0) {
+            ctx.reply("✅Операція видалення замовлення успішна✅");
+          } else 
+              throw new Error(result.retCode);
+        })
+        .catch((err) => {
+          ctx.reply("❌Помилка видалення замовлення");
+          console.log(err)
+        });
+    })
+  } else
+      ctx.reply("❌Помилка, функція не обрана❌")
+}
